b544a80f02062a0cbaee84e0ab26253094aea242
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-23 12:31:09 -0300

    feat: add simplified EVM interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use crate::opcodes::Opcode;
+
+mod opcodes;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let path = args.get(1).expect("No path provided").as_str();
+    let code = std::fs::read(path).expect("Could not read file");
+    let mut stack: Vec<u128> = Vec::new();
+    let mut pc = 0;
+    loop {
+        let Some(opcode) = code.get(pc).copied() else {
+            break;
+        };
+        println!("PC: {:04X} Opcode: {:02X}", pc, opcode);
+        match Opcode::from(opcode) {
+            Opcode::STOP => break,
+            Opcode::ADD => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                println!("Adding {a} and {b}, result: {}", a + b);
+                stack.push(a + b);
+            }
+            Opcode::PUSH1 => {
+                pc += 1;
+                let x = code[pc];
+                println!("Pushing {x} to stack");
+                stack.push(x as u128);
+            }
+            Opcode::UNUSED => panic!("Unknown opcode {:02X}", opcode),
+        }
+        pc += 1;
+    }
+    println!("Stack:");
+    println!("{:?}", stack);
 }
---
+++ src/opcodes.rs
@@ -0,0 +1,17 @@
+pub enum Opcode {
+    STOP = 0x00,
+    ADD = 0x01,
+    PUSH1 = 0x60,
+    UNUSED,
+}
+
+impl From<u8> for Opcode {
+    fn from(opcode: u8) -> Opcode {
+        match opcode {
+            0x00 => Opcode::STOP,
+            0x01 => Opcode::ADD,
+            0x60 => Opcode::PUSH1,
+            _ => Opcode::UNUSED,
+        }
+    }
+}
