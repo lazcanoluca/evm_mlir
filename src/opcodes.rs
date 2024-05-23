146a35f0393f28fa56f41ce8015cf2870e956037
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-23 16:44:19 -0300

    feat: parse bytecode

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -16,9 +16,34 @@ impl From<u8> for Opcode {
     }
 }
 
-
 pub enum Operation {
     Stop,
-    // Add {}
-    Push1 { value: u8 },
+    Add,
+    Push1(u8),
+}
+
+impl Operation {
+    pub fn from_bytecode(bytecode: Vec<u8>) -> Vec<Self> {
+        let mut operations = vec![];
+        let mut i = 0;
+
+        while i < bytecode.len() {
+            let Some(opcode) = bytecode.get(i).copied() else {
+                break;
+            };
+            let op = match Opcode::from(opcode) {
+                Opcode::STOP => Operation::Stop,
+                Opcode::ADD => Operation::Add,
+                Opcode::PUSH1 => {
+                    i += 1;
+                    let x = bytecode[i];
+                    Operation::Push1(x)
+                }
+                Opcode::UNUSED => panic!("Unknown opcode {:02X}", opcode),
+            };
+            operations.push(op);
+            i += 1;
+        }
+        operations
+    }
 }
