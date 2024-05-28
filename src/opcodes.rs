348637dd224daf676aa78d0e15d439709cc02cc2
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-28 18:11:38 -0300

    Remove unused code

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,8 +1,5 @@
 #[derive(Debug)]
 pub enum Opcode {
-    STOP = 0x00,
-    ADD = 0x01,
-    PUSH1 = 0x60,
     PUSH32 = 0x7F,
     UNUSED,
 }
@@ -10,9 +7,6 @@ pub enum Opcode {
 impl From<u8> for Opcode {
     fn from(opcode: u8) -> Opcode {
         match opcode {
-            x if x == Opcode::STOP as u8 => Opcode::STOP,
-            x if x == Opcode::ADD as u8 => Opcode::ADD,
-            x if x == Opcode::PUSH1 as u8 => Opcode::PUSH1,
             x if x == Opcode::PUSH32 as u8 => Opcode::PUSH32,
             _ => Opcode::UNUSED,
         }
@@ -23,8 +17,6 @@ impl From<u8> for Opcode {
 pub enum Operation {
     Stop,
     Add,
-    // TODO: [u8; 1]
-    Push1(u8),
     Push32([u8; 32]),
 }
 
@@ -38,14 +30,6 @@ impl Operation {
                 break;
             };
             let op = match Opcode::from(opcode) {
-                Opcode::STOP => Operation::Stop,
-                Opcode::ADD => Operation::Add,
-                Opcode::PUSH1 => {
-                    // TODO: move into a function pushN
-                    i += 1;
-                    let x = bytecode[i];
-                    Operation::Push1(x)
-                }
                 Opcode::PUSH32 => {
                     i += 1;
                     let x = bytecode[i..(i + 32)].try_into().unwrap();
