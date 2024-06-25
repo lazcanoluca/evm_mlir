2c4d8ff9ff375b754983a21551617bbf69cb4bbb
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-06-25 10:36:38 -0300

    chore: update MLIR env-vars with new version (#300)

--- scripts/env-macos.sh
+++ scripts/env-macos.sh
@@ -4,9 +4,9 @@
 # It sets the LLVM environment variables.
 export LIBRARY_PATH=/opt/homebrew/lib
 MLIR_SYS_180_PREFIX="$(brew --prefix llvm@18)"
-LLVM_SYS_180_PREFIX="$(brew --prefix llvm@18)"
+LLVM_SYS_181_PREFIX="$(brew --prefix llvm@18)"
 TABLEGEN_180_PREFIX="$(brew --prefix llvm@18)"
 
 export MLIR_SYS_180_PREFIX
-export LLVM_SYS_180_PREFIX
+export LLVM_SYS_181_PREFIX
 export TABLEGEN_180_PREFIX
